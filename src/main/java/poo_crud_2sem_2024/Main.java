dcfe52f701a512b32cd4c20f68754d17e23bfb59
Nattan Souza <150606760+EthanHueh@users.noreply.github.com>
2024-09-17 23:15:17 -0300

    Add files via upload

---
+++ src/main/java/poo_crud_2sem_2024/Main.java
@@ -0,0 +1,23 @@
+package poo_crud_2sem_2024;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        System.out.println("aaaa");
+        //Criar Entidade.java
+        
+        //Pessoa, cliente, funcionário, fornecedor
+        //Produto, fruta,
+        //Compra, Venda
+        
+        //10 atributos para a entidade
+        
+        //Cadastrar
+        //ConsultarTodos
+        //ConsultarByID
+        //Deletar
+        //Atualizar
+
+    }
+    
+}
