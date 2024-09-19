cdc447a89772b26673c4d4121b52e18b8d1f3a3e
EthanHueh <nattanssouzapvz@gmail.com>
2024-09-19 15:18:37 -0300

    Criacao da entidade construcao e delecao de comentarios na main

---
+++ src/main/java/poo_crud_2sem_2024/Construcao.java
@@ -0,0 +1,39 @@
+package poo_crud_2sem_2024;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Construcao {
+    
+    private int         id;
+    private String      nome;
+    private String      endereco;
+    private String      tipo;
+    private LocalDate   dataInicio;
+    private LocalDate   dataPrevisaoTermino;
+    private int         areaTotal_m2;
+    private double      orcamentoTotal;
+    private String      responsavel;
+    private String      status;
+    
+    public void cadastrar(){
+        
+    }
+    
+    public List<Construcao> consultarTodos(){
+        return null;
+    }
+    
+    public Construcao consultarByID(){
+        return null;
+    }
+    
+    public void atualizar(){
+        
+    }
+    
+    public void deletar(){
+        
+    }
+    
+}
--- src/main/java/poo_crud_2sem_2024/Main.java
+++ src/main/java/poo_crud_2sem_2024/Main.java
@@ -4,20 +4,6 @@ public class Main {
 
     public static void main(String[] args)  {
         System.out.println("aaaa");
-        //Criar Entidade.java
-        
-        //Pessoa, cliente, funcionário, fornecedor
-        //Produto, fruta,
-        //Compra, Venda
-        
-        //10 atributos para a entidade
-        
-        //Cadastrar
-        //ConsultarTodos
-        //ConsultarByID
-        //Deletar
-        //Atualizar.
-
     }
     
 }
